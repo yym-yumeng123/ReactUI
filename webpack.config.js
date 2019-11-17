20d25393ca5296d6c844c37de6a99737797f4224
yym-yumeng123 <18026493873@163.com>
2019-11-17 23:44:57 +0800

    feat(output): 配置webapck 输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
+const path = require('path')
 module.export = {
 	// 入口是 tsx, 但程序不认识 jsx, 配置 rules
   entry: {
 		idnex: './lib/index.tsx'
 	},
+	// 输出目录
+	output: {
+		// 因为不同的操作系统, 路径不一样, 所以使用 __dirname, 当前路径
+		path: path.resolve(__dirname, 'dist/lib'),
+		// 库的name
+		library: 'YUI',
+		// 库最后导出的格式
+		libraryTarget: 'umd'
+	},
 	module: {
 		rules: [
+			// 配置 ts tsx
 			{
 				test: /\.tsx?$/,
 				loader: 'awesome-typescript-loader'
