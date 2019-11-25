e458d95d8f6c1abe0aa694df009d5df9748e071c
yym-yumeng123 <18026493873@163.com>
2019-11-25 23:51:07 +0800

    fix(React.MouseEvent): React.MouseEvent onClick

--- lib/Icon/icon.tsx
+++ lib/Icon/icon.tsx
@@ -3,11 +3,12 @@ import "./importAllIcons";
 import "./icon.scss";
 interface IconProps {
   name: string;
+  onClick: (e: React.MouseEvent) => void;
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   return (
-    <svg className="yui-icon">
+    <svg className="yui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}></use>
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Icon from "./Icon/icon";
 
-ReactDOM.render(<Icon name="wechat" />, document.body.querySelector('#root'));
+const fn = (e: React.MouseEvent<SVGElement | SVGUseElement>) => {
+  console.log('fn...')
+  console.log(e)
+  console.log((e.target as SVGUseElement).href)  // ? as 断言
+}
+
+ReactDOM.render(<Icon name="wechat" onClick={fn} />, document.body.querySelector('#root'));
