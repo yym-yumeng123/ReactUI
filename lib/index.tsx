7af51fd91cd5e4c5a6fe743776d69947d43fbf54
yym-yumeng123 <18026493873@163.com>
2019-11-25 23:54:29 +0800

    feat(icon): onClick React.MouseEventhandler

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Icon from "./Icon/icon";
 
-const fn = (e: React.MouseEvent<SVGElement | SVGUseElement>) => {
+const fn: React.MouseEventHandler = (e) => {
   console.log('fn...')
-  console.log(e)
-  console.log((e.target as SVGUseElement).href)  // ? as 断言
+  console.log(e.target)
 }
 
 ReactDOM.render(<Icon name="wechat" onClick={fn} />, document.body.querySelector('#root'));
