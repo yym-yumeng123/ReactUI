00d7ad1d17cf92c9c0d3f340a0ddc68cd47be7a8
yym-yumeng123 <18026493873@163.com>
2019-11-27 00:16:13 +0800

    feat(Icon test): Icon 测试

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/test/__mocks__/file-mock.js",
-    // "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js"
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js"
   },
   // 测试文件在哪里
   testMatch: ["<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)"],
--- lib/Icon/icon.tsx
+++ lib/Icon/icon.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./importAllIcons";
 import "./icon.scss";
-import classes from '../helpers/classes'
+import classes from "../helpers/classes";
 
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = props => {
-  const { className, name, ...restProps } = props
+const Icon: React.FunctionComponent<IconProps> = ({
+  className,
+  name,
+  ...restProps
+}) => {
   return (
-    <svg className={classes('yui-icon', className)} {...restProps}>
+    <svg className={classes("yui-icon", className)} {...restProps}>
       <use xlinkHref={`#${name}`}></use>
     </svg>
   );
