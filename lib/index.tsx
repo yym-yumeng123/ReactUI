a796d58a4f105c11d99f7c29acf2ec4901d57eb3
yym-yumeng123 <18026493873@163.com>
2019-11-29 23:41:56 +0800

    feat(icon): 暴露 icon

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Icon from "./Icon/icon";
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log('fn...')
-  console.log(e.target)
-}
-
-ReactDOM.render(<Icon name="wechat" onClick={fn} onMouseEnter={() => console.log('enter')} />, document.body.querySelector('#root'));
+export { default as Icon } from "./Icon/icon";
