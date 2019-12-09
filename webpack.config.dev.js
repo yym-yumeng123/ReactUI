eec1d8ac9779bfe4e7358e41db954d8ba19bf0aa
yym-yumeng123 <18026493873@163.com>
2019-12-09 19:49:09 +0800

    feat(prettier): 添加格式化代码 prettier & stylelint 还是不能实施查看文件 (后面再改)

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,15 +1,20 @@
-const base = require("./webpack.config");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const base = require('./webpack.config');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StylelintWebpackPlugin = require('stylelint-webpack-plugin');
+
 module.exports = Object.assign({}, base, {
-  mode: "development",
+  mode: 'development',
   entry: {
-    example: './example.tsx'
+    example: './example.tsx',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "YUI",
-      template: "example.html"
-    })
-  ]
+      title: 'YUI',
+      template: 'example.html',
+    }),
+    new StylelintWebpackPlugin({
+      files: ['lib/**/*.s?(a|c)ss'],
+    }),
+  ],
 });
