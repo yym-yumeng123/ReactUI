1787f49afbebfe54aa85d3e26900958f9b315134
yym-yumeng123 <18026493873@163.com>
2019-12-13 23:28:07 +0800

    feat(dialog): html基本

--- lib/Dialog/dialog.tsx
+++ lib/Dialog/dialog.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import './dialog.scss';
+import Icon from '../Icon/icon'
 
 interface DialogProps {
   visible: boolean;
@@ -8,7 +9,19 @@ interface DialogProps {
 const Dialog: React.FunctionComponent<DialogProps> = props => {
   return props.visible ? (
     <Fragment>
-      <div className="yui-dialog">{props.children}</div>
+      <div className="yui-dialog">
+        <div className="yui-dialog-close">
+          <Icon name="close" />
+        </div>
+        <header className="yui-dialog-header">提示标题</header>
+        <main className="yui-dialog-main">
+          {props.children}
+        </main>
+        <footer className="yui-dialog-footer">
+          <button>确定</button>
+          <button>取消</button>
+        </footer>
+      </div>
       <div className="yui-dialog-mask"></div>
     </Fragment>
   ) : null;
