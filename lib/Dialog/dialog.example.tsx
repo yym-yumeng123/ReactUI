1a48613146581c4b93bb972e560ae93d94d0d611
yym-yumeng123 <18026493873@163.com>
2019-12-14 14:50:56 +0800

    feat(dialog): 添加事件 (有错误)

--- lib/Dialog/dialog.example.tsx
+++ lib/Dialog/dialog.example.tsx
@@ -6,7 +6,12 @@ export default function () {
   return (
     <>
       <button onClick={() => setvisible(!visible)}>按钮</button>
-      <Dialog visible={visible}>
+      <Dialog visible={visible} buttons={
+        [
+          <button onClick={() => {console.log('12');}}>确定1</button>,
+          <button onClick={() => {setvisible(false);}}>取消1</button>
+        ]
+      } onClose={() => {setvisible(false);;}}>
         <strong>
           hi
         </strong>
--- lib/Dialog/dialog.tsx
+++ lib/Dialog/dialog.tsx
@@ -1,19 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, ReactElement } from 'react';
 import './dialog.scss';
 import { Icon } from '../index';
 import { scopedClassMaker } from '../utils/classes';
 
 interface DialogProps {
   visible: boolean;
+  buttons: Array<ReactElement>;
+  onClose: React.MouseEventHandler;
 }
 
 const scopedClass = scopedClassMaker('yui-dialog');
 
 const Dialog: React.FunctionComponent<DialogProps> = props => {
+  const handlerClose: React.MouseEventHandler = (event) => {
+    props.onClose(event);
+  };
+
   return !props.visible ? (
     <Fragment>
       <div className={scopedClass()}>
-        <div className={scopedClass('close')}>
+        <div className={scopedClass('close')} onClick={handlerClose}>
           <Icon name="close" />
         </div>
         <header className={scopedClass('header')}>提示标题</header>
@@ -21,8 +27,7 @@ const Dialog: React.FunctionComponent<DialogProps> = props => {
           {props.children}
         </main>
         <footer className={scopedClass('footer')}>
-          <button>确定</button>
-          <button>取消</button>
+          {props.buttons}
         </footer>
       </div>
       <div className={scopedClass('mask')}></div>
