10aeb856977a26392d956e2337ca1b85123a5c36
yym-yumeng123 <18026493873@163.com>
2020-03-01 23:38:59 +0800

    feat(demo): 优化demo , 新增源代码高亮

--- demo.tsx
+++ demo.tsx
@@ -1,14 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
 interface Props {
   code: string;
 }
 
 const Demo: React.FunctionComponent<Props> = props => {
+  const [codeVisible, setCodeVisible] = useState(false);
+  const code = (
+    <Highlight {...defaultProps} code={props.code} language="jsx">
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            // tslint:disable-next-line: jsx-key
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                // tslint:disable-next-line: jsx-key
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
   return (
     <div>
-      {props.children}
-      <pre>{props.code}</pre>
+      <div>{props.children}</div>
+      <div>
+        <button onClick={() => setCodeVisible(!codeVisible)}>查看代码</button>
+        {codeVisible && code}
+      </div>
     </div>
   );
 };
