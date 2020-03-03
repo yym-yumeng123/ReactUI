cdc79d78241cdc20e6da2b1514cf17a1c00171a0
yym-yumeng123 <18026493873@163.com>
2020-03-03 21:08:56 +0800

    feat(webpack):新增 yarn doc 文档配置

---
+++ webpack.config.doc.js
@@ -0,0 +1,18 @@
+const base = require('./webpack.config')
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = Object.assign({}, base, {
+  mode: "production",
+  entry: {
+    example: './example.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'doc')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'example.html',
+      filename: 'example.html'
+    }),
+  ],
+});
