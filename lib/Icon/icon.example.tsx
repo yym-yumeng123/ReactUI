8d4fc7eadf9de2d51ff2f96f0dc8733d9c3799bb
yym-yumeng123 <18026493873@163.com>
2020-09-27 23:59:07 +0800

    icon 事件 ts 问题

--- lib/Icon/icon.example.tsx
+++ lib/Icon/icon.example.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import Icon from "./icon";
 
 const IconExample: React.FunctionComponent = () => {
+  const fn = (e: React.MouseEvent) => {
+    console.log(e);
+    console.log(e.target);
+
+    // as 把某个东西当做什么类处理
+    console.log((e.target as SVGUseElement).href);
+    console.log((e.target as HTMLDivElement).style);
+  }
+
   return (
     <div>
       <Icon name="qq" />
@@ -9,6 +18,7 @@ const IconExample: React.FunctionComponent = () => {
       <Icon name="wechat" />
       <Icon name="view" />
       <Icon name="view_off" />
+      <Icon name="upload" onClick={fn} />
     </div>
   );
 };
--- lib/Icon/icon.tsx
+++ lib/Icon/icon.tsx
@@ -10,6 +10,8 @@ import classes from "../helpers/classes";
 // 声明 icon 属性
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
+  // svg 元素的鼠标事件处理函数
+  // onClick?: React.MouseEventHandler<SVGElement>
 }
 
 // icon 是一个函数组件, 属性类型是 iconprops
