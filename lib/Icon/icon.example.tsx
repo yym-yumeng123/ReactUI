58f0fb73d6ac9940f7937753044e952dc6a50ac7
yym-yumeng123 <18026493873@163.com>
2020-09-28 23:13:52 +0800

    Icon add  color size

--- lib/Icon/icon.example.tsx
+++ lib/Icon/icon.example.tsx
@@ -3,23 +3,16 @@ import Icon from "./icon";
 
 const IconExample: React.FunctionComponent = () => {
   const fn: React.MouseEventHandler = (e) => {
-
     console.log(e);
     console.log(e.target);
-
-    // as 把某个东西当做什么类处理
-    // as 断言
-    console.log((e.target as SVGUseElement).href);
-    console.log((e.target as HTMLDivElement).style);
   }
 
   return (
     <div>
-      <Icon name="qq" />
-      <Icon name="alipay" />
-      <Icon name="wechat" />
       <Icon name="view" />
-      <Icon name="view_off" />
+      <Icon name="view_off" className="icon" />
+      <Icon name="edit" className="icon" color="red" />
+      <Icon name="close" className="icon" color="red" size="20px" />
       <Icon name="upload" onClick={fn} />
     </div>
   );
--- lib/Icon/icon.tsx
+++ lib/Icon/icon.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "./importAllIcons";
-import "./icon.scss";
 import classes from "../helpers/classes";
+import "./icon.scss";
 
-/**
- * @params {name} icon name
- */
-
-// 声明 icon 属性
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
-  // svg 元素的鼠标事件处理函数
-  // onClick?: React.MouseEventHandler<SVGElement>
+  color?: string;
+  size?: string;
 }
 
-// icon 是一个函数组件, 属性类型是 iconprops
 const Icon: React.FunctionComponent<IconProps> = ({
   className,
   name,
+  color,
+  size,
   ...restProps
 }) => {
   return (
-    <svg className={classes("yui-icon", className)} {...restProps}>
+    <svg
+      style={{ fill: color, fontSize: size, }}
+      className={classes("yui-icon", className)}
+      {...restProps}
+    >
       <use xlinkHref={`#${name}`}></use>
     </svg>
   );
