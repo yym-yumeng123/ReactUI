0d1bdcab3d0cfd5e81e1ce8529a7f6eaf5b3bdfc
yym-yumeng123 <18026493873@163.com>
2020-10-19 21:21:43 +0800

    添加 button 属性, 以及 css 样式

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import Button from './button'
+import Button from "./button";
 
 const ButtonExample: React.FunctionComponent = () => {
-  return <div><Button>按钮</Button></div>;
+  return (
+    <div>
+      <Button>按钮</Button>
+      <Button lever="primary">提醒按钮</Button>
+      <Button lever="danger">提醒按钮</Button>
+      <Button lever="link" href="baidu.com">链接按钮</Button>
+      <Button lever="primary" disabled>链接按钮</Button>
+
+      <div>
+        <br/>
+        <Button lever="primary" block>block 按钮</Button>
+      </div>
+    </div>
+  );
 };
 
 export default ButtonExample;
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,37 +1,54 @@
 import * as React from "react";
 import classNames from "classnames";
+import "./button.scss";
 
 export enum ButtonSize {
   Large = "lg",
-  Small = "sm",
+  Small = "sm"
 }
 
 export enum ButtonType {
   Default = "default",
   Primary = "primary",
   Danger = "danger",
-  Link = "link",
+  Link = "link"
 }
 
 interface ButtonProps {
   children: React.ReactNode;
-  btnType?: ButtonType;
+  lever?: 'primary' | 'danger' | 'link';
   size?: ButtonSize;
   disabled?: boolean;
   className?: string;
   href?: string;
+  block?: boolean;
+  onClick?: () => void
 }
 
-const Button: React.FC<ButtonProps> = (props) => {
-  const { children, btnType, size, disabled } = props;
+const Button: React.FC<ButtonProps> = props => {
+  const { children, lever, size, disabled, href, onClick, block } = props;
 
   // class 不同属性
   const classes = classNames("yui-button", {
-    [`yui-button-${btnType}`]: btnType,
+    [`yui-button-${lever}`]: lever,
     [`yui-button-${size}`]: size,
-    disabled: btnType === ButtonType.Link && disabled,
+    ['yui-button-disabled']: disabled,
+    ['yui-button-block']: block,
+
   });
-  return <div>{children}</div>;
+  return (
+    <>
+      {lever === "link" ? (
+        <a href={href} target="_blank" className={classes}>
+          {children}
+        </a>
+      ) : (
+        <button onClick={onClick} className={classes}>
+          {children}
+        </button>
+      )}
+    </>
+  );
 };
 
 export default Button;
