a3cd7b3e23612ed24c2a19665b37349d368287e7
yym-yumeng123 <18026493873@163.com>
2020-12-09 15:17:13 +0800

    给没一级 tree item 加一个 level 判断等级

--- lib/Tree/Tree.tsx
+++ lib/Tree/Tree.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 interface SourceDataItem {
   title: string;
-  value: string;
-  children?: SourceDataItem[];
+  key: string;
+  children?: SourceDataItem[] | undefined | null;
 }
 interface TreeProps {
   sourceData: SourceDataItem[];
@@ -12,22 +12,27 @@ interface TreeProps {
 const Tree: React.FC<TreeProps> = props => {
   const { sourceData } = props;
 
-  // 递归 children
-  const TreeItem = (item: SourceDataItem) => {
+  /**
+   * 递归渲染树的每一项
+   * @param item 数组的每一项
+   * @param level 处于第几层, 默认为0层
+   */
+  const renderItem = (item: SourceDataItem, level = 0) => {
     return (
-      <div key={item.value}>
-        { item.title }
+      <div key={item.key} style={{ paddingLeft: level * 10 + "px" }}>
+        {item.title}
         {item.children?.map(subItem => {
-          return TreeItem(subItem)
+          // 每次渲染 级别 + 1
+          return renderItem(subItem, level + 1);
         })}
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div>
       {sourceData.map(item => {
-        return TreeItem(item)
+        return renderItem(item);
       })}
     </div>
   );
--- lib/Tree/tree.example.tsx
+++ lib/Tree/tree.example.tsx
@@ -3,21 +3,28 @@ import Tree from "./Tree";
 const TreeDemo = () => {
   const [treeArray] = useState([
     {
-      title: '一',
-      value: '1',
+      title: "一",
+      key: "1",
       children: [
-        { title: '一之一', value: '1.1'},
-        { title: '一之二', value: '1.2'}
+        {
+          title: "一之一",
+          key: "1.1",
+          children: [
+            { title: "一之一之一", key: "1.1.1" },
+            { title: "一之一之二", key: "1.1.2" }
+          ]
+        },
+        { title: "一之二", key: "1.2" }
       ]
     },
     {
-      title: '二',
-      value: '2',
+      title: "二",
+      key: "2",
       children: [
-        { title: '二之一', value: '2.1'},
-        { title: '二之二', value: '2.2'}
+        { title: "二之一", key: "2.1" },
+        { title: "二之二", key: "2.2" }
       ]
-    },
+    }
   ]);
   return (
     <div>
