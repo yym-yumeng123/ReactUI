278ac81ee7120c6934f9d690f28c1f2e2345519c
yym-yumeng123 <18026493873@163.com>
2020-12-09 11:41:23 +0800

    添加 tree 数据 sourceData

--- lib/Tree/Tree.tsx
+++ lib/Tree/Tree.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
-const Tree = (props: any) => {
-  return <div>Tree</div>;
+interface SourceDataItem {
+  title: string;
+  value: string;
+  children?: SourceDataItem[];
+}
+interface TreeProps {
+  sourceData: SourceDataItem[];
+}
+
+const Tree: React.FC<TreeProps> = props => {
+  const { sourceData } = props;
+
+  const TreeItem = (item: SourceDataItem) => {
+    return (
+      <div key={item.value}>
+        { item.title }
+        {item.children?.map(subItem => {
+          return TreeItem(subItem)
+        })}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {sourceData.map(item => {
+        return TreeItem(item)
+      })}
+    </div>
+  );
 };
 
 export default Tree;
--- lib/Tree/tree.example.tsx
+++ lib/Tree/tree.example.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Tree from "./Tree";
 const TreeDemo = () => {
-  return <div>Tree</div>;
+  const [treeArray] = useState([
+    {
+      title: '一',
+      value: '1',
+      children: [
+        { title: '一之一', value: '1.1'},
+        { title: '一之二', value: '1.2'}
+      ]
+    },
+    {
+      title: '二',
+      value: '2',
+      children: [
+        { title: '二之一', value: '2.1'},
+        { title: '二之二', value: '2.2'}
+      ]
+    },
+  ]);
+  return (
+    <div>
+      <Tree sourceData={treeArray} />
+    </div>
+  );
 };
 export default TreeDemo;
