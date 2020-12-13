ebcf3bfa672edb98adf3ca69cd348daee9ae71d6
yym-yumeng123 <18026493873@163.com>
2020-12-13 22:43:24 +0800

    重构 classes 并测试

--- lib/utils/classes.ts
+++ lib/utils/classes.ts
@@ -2,6 +2,12 @@ interface Options {
   extra: string;
 }
 
+// 定义 class 开关
+interface ClassToggles {
+  // 类似 hasAside : true
+  [K: string]: boolean;
+}
+
 /**
  * @description 命名不同的类名了解
  * @param name 每一个不同的连接
@@ -25,8 +31,22 @@ function classes(...names: Array<string | undefined | boolean>) {
  * @description 合并 上面两个函数
  */
 const mergeClassOrScoped = (prefix: string) => {
-  return (name?: string, options?: Options) => {
-    const result = [prefix, name].filter(Boolean).join("-");
+  return (name?: string | ClassToggles, options?: Options) => {
+    let name2;
+    let result;
+    // 如果 name 为 string, 直接拼接
+    if (typeof name === "string" || name === undefined) {
+      name2 = name;
+      result = [prefix, name2].filter(Boolean).join("-");
+    } else {
+      name2 = Object.entries(name)
+        .filter(item => item[1])
+        .map(kv => kv[0]);
+      result = name2
+        .map(item => [prefix, item].filter(Boolean).join("-"))
+        .join(" ");
+    }
+
     if (options?.extra) {
       return [result, options?.extra].filter(Boolean).join(" ");
     } else {
