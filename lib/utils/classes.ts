4349eb20a05ca33e3daea7b9018ba610a240f5f4
yym-yumeng123 <18026493873@163.com>
2020-12-13 23:21:48 +0800

    去掉 大括号

--- lib/utils/classes.ts
+++ lib/utils/classes.ts
@@ -30,24 +30,16 @@ function classes(...names: Array<string | undefined | boolean>) {
 /**
  * @description 合并 上面两个函数
  */
-const mergeClassOrScoped = (prefix: string) => {
-  return (name: string | ClassToggles, options?: Options) => {
-    const namesObject =
-      (typeof name === "string" || name === undefined) ? { [name]: name } : name;
-
-    // 对象转为数组 => 过滤 => 循环 => 每个类名加空格
-    const scoped = Object.entries(namesObject)
-      .filter(item => item[1] !== false)
-      .map(kv => kv[0])
-      .map(item => [prefix, item].filter(Boolean).join("-"))
-      .join(" ");
-
-    if (options?.extra) {
-      return [scoped, options?.extra].filter(Boolean).join(" ");
-    } else {
-      return scoped;
-    }
-  };
-};
+const mergeClassOrScoped = (prefix: string) => (
+  name: string | ClassToggles,
+  options?: Options
+) =>
+  // 对象转为数组 => 过滤 => 循环 => 每个类名加空格
+  Object.entries(name instanceof Object ? name : { [name]: name })
+    .filter(item => item[1] !== false)
+    .map(kv => kv[0])
+    .map(item => [prefix, item].filter(Boolean).join("-"))
+    .concat(options?.extra || [])
+    .join(" ");
 
 export { scopedClassMaker, classes, mergeClassOrScoped };
