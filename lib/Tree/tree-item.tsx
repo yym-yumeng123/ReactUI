cbf4d379f71bbf52288699be43fa0432bca493cc
yym-yumeng123 <18026493873@163.com>
2020-12-30 22:46:26 +0800

    自定义 hook & 优化

--- lib/Tree/tree-item.tsx
+++ lib/Tree/tree-item.tsx
@@ -1,13 +1,9 @@
-import React, {
-  ChangeEventHandler,
-  InputHTMLAttributes,
-  useRef,
-  useState
-} from "react";
+import React, { ChangeEventHandler, useRef, useState } from "react";
 import Icon from "lib/Icon/icon";
 import { addPrefixAndscopedClassMarker } from "../utils/classes";
-import { flatten } from "../utils/utils";
+import { flatten, intersect } from "../utils/utils";
 import useUpdateCollapse from "lib/hooks/useUpdateCollapse";
+import useToggle from "lib/hooks/useToggle";
 const sc = addPrefixAndscopedClassMarker("yui-tree");
 
 interface TreeItemProps {
@@ -16,6 +12,19 @@ interface TreeItemProps {
   treeProps: TreeProps;
   onItemChange: (value: string[]) => void;
 }
+
+/**
+ * @description 收集所有子元素
+ * @param item 每一项元素
+ */
+const collectChildrenValues = (item: SourceDataItem): string[] => {
+  return flatten(
+    item.children?.map(subItem => [
+      subItem.title,
+      collectChildrenValues(subItem)
+    ])
+  );
+};
 
 /**
  * 递归渲染树的每一项
@@ -31,8 +40,9 @@ const TreeItem: React.FC<TreeItemProps> = props => {
     treeProps: { multiple, selected, onChange }
   } = props;
   // 展开
-  const [expanded, setExpanded] = useState(true);
+
   const divRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const classes = {
     [`level-${level}`]: true,
@@ -43,85 +53,10 @@ const TreeItem: React.FC<TreeItemProps> = props => {
     ? selected.indexOf(item.title) >= 0
     : selected === item.title;
 
-  /**
-   * @description 收集所有子元素
-   * @param item 每一项元素
-   */
-  const collectChildrenValues = (item: SourceDataItem): string[] => {
-    return flatten(
-      item.children?.map(subItem => [
-        subItem.title,
-        collectChildrenValues(subItem)
-      ])
-    );
-  };
+  const { expanded, expand, collapse } = useToggle(true);
+  console.log(expand, 'expand....');
 
-  const onSelectChange: ChangeEventHandler<HTMLInputElement> = e => {
-    // chidren 的值
-    const childrenValues = collectChildrenValues(item);
 
-    if (multiple) {
-      if (e.target.checked) {
-        onItemChange([...selected, item.title, ...childrenValues]);
-      } else {
-        onItemChange(
-          selected.filter(
-            value =>
-              value !== item.title && childrenValues.indexOf(value) === -1
-          )
-        );
-      }
-    } else {
-      if (e.target.checked) {
-        onChange(item.title);
-      } else {
-        onChange("");
-      }
-    }
-  };
-
-  /**
-   *
-   * @param 交集
-   */
-  function intersect<T>(array1: T[], array2: T[]): T[] {
-    const result: T[] = [];
-    array1.map(item => {
-      if (array2.indexOf(item) >= 0) {
-        result.push(item);
-      }
-    });
-    return result;
-  }
-
-  // 子元素变化
-  const onItemChange1 = (values: any) => {
-    // 子代被全部选中
-    const childrenValues = collectChildrenValues(item);
-    console.log(childrenValues, "dslk");
-
-    const common = intersect(values, childrenValues);
-    console.log(common, "带领僧松开");
-    if (common.length !== 0) {
-      onItemChange(Array.from(new Set(values.concat(item.title))));
-      if (common.length === childrenValues.length) {
-        inputRef.current!.indeterminate = false
-      } else {
-        inputRef.current!.indeterminate = true
-      }
-    } else {
-      onItemChange(values.filter((v: string) => v !== item.title))
-      inputRef.current!.indeterminate = false
-    }
-  };
-
-  const collapse = () => {
-    setExpanded(false);
-  };
-
-  const expand = () => {
-    setExpanded(true);
-  };
 
   useUpdateCollapse(expanded, () => {
     if (!divRef.current) return;
@@ -158,7 +93,46 @@ const TreeItem: React.FC<TreeItemProps> = props => {
     }
   });
 
-  const inputRef = useRef<HTMLInputElement>(null);
+  const onSelectChange: ChangeEventHandler<HTMLInputElement> = e => {
+    // chidren 的值
+    const childrenValues = collectChildrenValues(item);
+
+    if (multiple) {
+      if (e.target.checked) {
+        onItemChange([...selected, item.title, ...childrenValues]);
+      } else {
+        onItemChange(
+          selected.filter(
+            value =>
+              value !== item.title && childrenValues.indexOf(value) === -1
+          )
+        );
+      }
+    } else {
+      if (e.target.checked) {
+        onChange(item.title);
+      } else {
+        onChange("");
+      }
+    }
+  };
+
+  // 子元素变化
+  const onItemChange1 = (values: any) => {
+    // 子代被全部选中
+    const childrenValues = collectChildrenValues(item);
+    console.log(childrenValues, "dslk");
+
+    const common = intersect(values, childrenValues);
+    console.log(common, "带领僧松开");
+    if (common.length !== 0) {
+      onItemChange(Array.from(new Set(values.concat(item.title))));
+      inputRef.current!.indeterminate = common.length !== childrenValues.length;
+    } else {
+      onItemChange(values.filter((v: string) => v !== item.title));
+      inputRef.current!.indeterminate = false;
+    }
+  };
 
   return (
     <div key={item.key} className={sc(classes)}>
--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -15,5 +15,17 @@ const flatten = (array: RecursiveArray<string>): string[] => {
   );
 };
 
+/**
+ * @param 交集
+ */
+function intersect<T>(array1: T[], array2: T[]): T[] {
+  const result: T[] = [];
+  array1.map(item => {
+    if (array2.indexOf(item) >= 0) {
+      result.push(item);
+    }
+  });
+  return result;
+}
 
-export { flatten }
+export { flatten, intersect };
