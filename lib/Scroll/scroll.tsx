3f3d1b520ce4d11e0e2e1c5c1e820b79212ffbf9
yym-yumeng123 <18026493873@163.com>
2021-01-06 16:57:41 +0800

    滚动条可以拖动

--- lib/Scroll/scroll.tsx
+++ lib/Scroll/scroll.tsx
@@ -1,4 +1,6 @@
-import React, {
+import React from "react";
+import {
+  MouseEventHandler,
   HTMLAttributes,
   UIEventHandler,
   useEffect,
@@ -16,11 +18,19 @@ interface ScrollProps extends HTMLAttributes<HTMLDivElement> {}
 const Scroll: React.FC<ScrollProps> = props => {
   const { children, ...restProps } = props;
   const [barHeight, setBarHeight] = useState(0);
-  const [barTopHeight, setBarTopHeight] = useState(0);
+  const [barTopHeight, _setBarTopHeight] = useState(0);
   const containerRef = useRef<HTMLDivElement>(null);
 
+  const setBarTopHeight = (number: number) => {
+    const { current } = containerRef;
+    const scrollHeight = current!.scrollHeight;
+    const viewHeight = current!.getBoundingClientRect().height;
+    const maxBarTop = ((scrollHeight - viewHeight) * viewHeight) / scrollHeight;
+    if (number < 0 || number > maxBarTop) return;
+    _setBarTopHeight(number);
+  };
+
   const onScroll: UIEventHandler = e => {
-    console.log(e.currentTarget.scrollTop, e.currentTarget.scrollHeight);
     const scrollHeight = e.currentTarget.scrollHeight;
     // 可见视图向上滚动的距离
     const scrollTop = e.currentTarget.scrollTop;
@@ -36,7 +46,7 @@ const Scroll: React.FC<ScrollProps> = props => {
    * @description 第一次进来计算 bar 的高度
    */
   useEffect(() => {
-    const { current } = containerRef
+    const { current } = containerRef;
     // 视图包含 超出的高度
     const scrollHeight = current!.scrollHeight;
     // 可视高度
@@ -44,6 +54,33 @@ const Scroll: React.FC<ScrollProps> = props => {
     // 滚动滑块的高度
     const barHeight = (viewHeight * viewHeight) / scrollHeight;
     setBarHeight(barHeight);
+  }, []);
+
+  const draggingRef = useRef(false);
+  const startYRef = useRef(0);
+  const startBarTopRef = useRef(0);
+  const onMouseDownBar: MouseEventHandler = e => {
+    draggingRef.current = true;
+    startYRef.current = e.clientY;
+    startBarTopRef.current = barTopHeight;
+  };
+  const onMouseMoveBar = (e: MouseEvent) => {
+    if (draggingRef.current) {
+      const delta = e.clientY - startYRef.current;
+      setBarTopHeight(delta + startBarTopRef.current);
+    }
+  };
+  const onMouseUpBar = () => {
+    draggingRef.current = false;
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", onMouseMoveBar);
+    document.addEventListener("mouseup", onMouseUpBar);
+    return () => {
+      document.removeEventListener("mousemove", onMouseMoveBar);
+      document.removeEventListener("mouseup", onMouseUpBar);
+    }
   }, []);
 
   return (
@@ -59,8 +96,12 @@ const Scroll: React.FC<ScrollProps> = props => {
       {/* 滚动条 */}
       <div className={prefix("track")}>
         <div
+          onMouseDown={onMouseDownBar}
           className={prefix("bar")}
-          style={{ height: barHeight, transform: `translateY(${barTopHeight}px)`, }}
+          style={{
+            height: barHeight,
+            transform: `translateY(${barTopHeight}px)`
+          }}
         ></div>
       </div>
     </div>
