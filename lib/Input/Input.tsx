c6aad8139e23638f675c19bd89849c411f3903b9
yym-yumeng123 <18026493873@163.com>
2021-01-22 00:23:50 +0800

    初始化 input

--- lib/Input/Input.tsx
+++ lib/Input/Input.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, {FC, InputHTMLAttributes, ReactElement} from "react";
+// import Icon from "lib/Icon/icon";
 
-const Input = () => {
+// 定义 size
+type InputSize = "lg" | "sm";
+
+/**
+ * 希望几个 HTML input 所有属性
+ * 原有属性已经有 size, 我们再次定义, 会有冲突, 怎么解决?
+ * 1. 修改名称
+ * 2. 使用 Omit
+ */
+export interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, "size"> {
+  disabled?: boolean;
+  size?: InputSize;
+  icon?: string;
+  prepand?: string | ReactElement;
+  append?: string | ReactElement;
+}
+
+const Input: FC<InputProps> = (props) => {
+  const {disabled, size, icon, prepand, append, ...restProps} = props;
+  // 根据属性计算不同的 className
   return (
-    <div>Input</div>
+    <>
+      <input type="text" {...restProps}/>
+    </>
   );
 };
 
