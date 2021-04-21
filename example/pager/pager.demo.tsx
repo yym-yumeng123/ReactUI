68fd8342c110f1c46011dc3c6f87b0ecbad44126
yym-yumeng123 <18026493873@163.com>
2021-04-21 16:51:40 +0800

    过滤数组, 基本完善css

--- example/pager/pager.demo.tsx
+++ example/pager/pager.demo.tsx
@@ -4,7 +4,9 @@ import Pager from "lib/Pager/pager";
 const PagerDemo = () => {
   return (
     <div>
-      <Pager totalPage={20001} current={3} />
+      <Pager totalPage={20001} current={1} />
+      <br/>
+      <Pager totalPage={200} current={3} />
     </div>
   );
 };
--- lib/Pager/pager.tsx
+++ lib/Pager/pager.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
+import Icon from "lib/Icon/icon";
 import { unique } from "../utils/utils";
 import { addPrefixAndscopedClassMarker } from "../utils/classes";
 import "./pager.scss";
@@ -24,7 +25,7 @@ const Pager: FC<PagerProps> = props => {
   ]);
 
   useEffect(() => {
-    const sortAndUniquePages = unique(pages.sort((a, b) => a - b));
+    const sortAndUniquePages = unique(pages.filter(i => i >=1 && i <= totalPage).sort((a, b) => a - b));
     const spreadPages = sortAndUniquePages.reduce(
       (prev: Array<any>, cur, index, arr) => {
         prev.push(cur);
@@ -41,21 +42,30 @@ const Pager: FC<PagerProps> = props => {
     console.log("122", spreadPages);
   }, []);
 
+  const pagerElement = pages.map(item => {
+    if (item === current) {
+      return (
+        <span
+          className={prefix({
+            item: true,
+            current: current === item
+          })}
+        >
+          {item}
+        </span>
+      );
+    } else if (item === "...") {
+      return <span className={prefix("separator")}>...</span>;
+    } else {
+      return <span className={prefix("item")}>{item}</span>;
+    }
+  });
+
   return (
     <div className={prefix("")}>
-      {pages.map(item => {
-        return (
-          <span
-            className={prefix({
-              item: true,
-              current: current === item,
-              separator: item === "..."
-            })}
-          >
-            {item}
-          </span>
-        );
-      })}
+      <Icon name="page_first" size="12" />
+      {pagerElement}
+      <Icon name="page_last" size="12" />
     </div>
   );
 };
