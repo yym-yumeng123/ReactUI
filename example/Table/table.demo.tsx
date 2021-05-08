d2b63f6434047e5a4f2d9f4a90482187cf6333b5
yym-yumeng123 <18026493873@163.com>
2021-05-08 16:51:10 +0800

    实现最简单的table 展示

--- example/Table/table.demo.tsx
+++ example/Table/table.demo.tsx
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Table from "lib/Table/table";
 
 const TableDemo = () => {
-  const [columns, setcColumns] = useState<Array<any>>([]);
-  const [dataSource, setDataSource] = useState<Array<any>>([]);
+  const [columns, setcColumns] = useState([
+    {
+      title: "姓名",
+      key: "name",
+    },
+    {
+      title: "年龄",
+      key: "age"
+    },
+    {
+      title: "住址",
+      key: "address"
+    }
+  ]);
+  const [dataSource, setDataSource] = useState<Array<any>>([
+    {
+      key: "1",
+      name: "胡彦斌",
+      age: 32,
+      address: "西湖区湖底公园1号"
+    },
+    {
+      key: "2",
+      name: "胡彦祖",
+      age: 42,
+      address: "西湖区湖底公园1号"
+    }
+  ]);
+
   return (
     <div>
       <Table columns={columns} dataSource={dataSource} />
--- lib/Table/table.tsx
+++ lib/Table/table.tsx
@@ -1,16 +1,54 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 import { addPrefixAndscopedClassMarker } from "../utils/classes";
 
 const prefix = addPrefixAndscopedClassMarker("yui-table");
 
+interface Columns {
+  title: string;
+  key: string;
+}
+
 interface TableProps {
-  columns: any[];
-  dataSource: any[]
+  columns: Columns[];
+  dataSource: any[];
 }
 
-const Table: FC<TableProps> = (props) => {
-  const { columns, dataSource } = props
-  return <div className={prefix('')}>Table</div>
-}
+const Table: FC<TableProps> = props => {
+  const { columns, dataSource } = props;
+  console.log(columns, dataSource);
 
-export default Table
+  return (
+    <div className={prefix("")}>
+      <table>
+        <thead>
+          <tr>
+            <th>序号</th>
+            {columns.map(item => {
+              return <th>{item.title}</th>;
+            })}
+          </tr>
+        </thead>
+
+        <tbody>
+          {dataSource.map((item, index) => {
+            return (
+              <tr key={item.key}>
+                <td>{index}</td>
+                {columns.map(column => {
+                  return (
+                    <>
+                      <td>{item[column.key]}</td>
+                    </>
+                  );
+                })}
+              </tr>
+            );
+          })}
+          <tr></tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
