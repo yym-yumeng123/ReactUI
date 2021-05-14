8856bd5c2e3c0a5207cfb316c5674b281123714d
yym-yumeng123 <18026493873@163.com>
2021-05-14 15:51:48 +0800

    添加样式 & add checked props

--- lib/Radio/radio.tsx
+++ lib/Radio/radio.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { ChangeEvent, ChangeEventHandler, FC, InputHTMLAttributes, useState } from 'react';
 import { addPrefixAndscopedClassMarker } from "../utils/classes";
 import './radio.scss'
 
 const prefix = addPrefixAndscopedClassMarker("yui-radio");
 
-const Radio = () => {
+
+interface RadioProps {
+  checked?: boolean
+}
+
+const Radio: FC<RadioProps> = props => {
+  const { checked = false } = props
+  const [defaultChecked, setDefaultChecked] = useState(checked);
+
+  const chengeStatus = (e: any) => {
+    const { checked } = e.target
+    setDefaultChecked(checked)
+  }
+
   return (
     <label className={prefix('wrapper')}>
       <span className={prefix('')}>
-        <span className={prefix('inner')}></span>
-        <input type="radio" className={prefix('input')} />
+        <span className={prefix({'inner': true, checked: defaultChecked})}></span>
+        <input type="radio" checked={defaultChecked} onChange={chengeStatus} className={prefix('input')} />
       </span>
       <span className={prefix('label')}>Radio</span>
     </label>
