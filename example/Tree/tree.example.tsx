60f0da80c8a5c9ba9f9f7c780f28e2bfd13c2b9b
yym-yumeng123 <18026493873@163.com>
2021-11-01 14:51:27 +0800

    update doc 后 先把 类型错误的 tree 还原, 后面解决

--- example/Tree/tree.example.tsx
+++ example/Tree/tree.example.tsx
@@ -1,63 +1,63 @@
-// import React, {useState} from "react";
-// import Tree from "lib/Tree/Tree";
+import React, {useState} from "react";
+import Tree from "lib/Tree/Tree";
 
-// const TreeDemo = () => {
-//   const [treeArray] = useState([
-//     {
-//       title: "1",
-//       key: "1",
-//       children: [
-//         {
-//           title: "1.1",
-//           key: "1.1",
-//           children: [
-//             {title: "一之一之一", key: "1.1.1"},
-//             {title: "一之一之二", key: "1.1.2"}
-//           ]
-//         },
-//         {title: "1.2", key: "1.2"}
-//       ]
-//     },
-//     {
-//       title: "二",
-//       key: "2",
-//       children: [
-//         {title: "二之一", key: "2.1"},
-//         {title: "二之二", key: "2.2"}
-//       ]
-//     }
-//   ]);
+const TreeDemo = () => {
+  const [treeArray] = useState([
+    {
+      title: "1",
+      key: "1",
+      children: [
+        {
+          title: "1.1",
+          key: "1.1",
+          children: [
+            {title: "一之一之一", key: "1.1.1"},
+            {title: "一之一之二", key: "1.1.2"}
+          ]
+        },
+        {title: "1.2", key: "1.2"}
+      ]
+    },
+    {
+      title: "二",
+      key: "2",
+      children: [
+        {title: "二之一", key: "2.1"},
+        {title: "二之二", key: "2.2"}
+      ]
+    }
+  ]);
 
-//   const [selecteValues, setSelecteValues] = useState<string[]>([]);
-//   const [selectedValue, setSelecteValue] = useState("1.1");
-//   const onChange = (val: string[]) => {
-//     console.log(val, "val");
-//     setSelecteValues(val);
-//   };
+  const [selecteValues, setSelecteValues] = useState<string[]>([]);
+  const [selectedValue, setSelecteValue] = useState("1.1");
+  const onChange = (val: string[]) => {
+    console.log(val, "val");
+    setSelecteValues(val);
+  };
 
-//   const onChange1 = (val: string) => {
-//     console.log(val, "val");
-//     setSelecteValue(val);
-//   };
+  const onChange1 = (val: string) => {
+    console.log(val, "val");
+    setSelecteValue(val);
+  };
 
-//   return (
-//     <div style={{width: 200,}}>
-//       {selecteValues.join(", ")}
-//       <Tree
-//         sourceData={treeArray}
-//         onChange={onChange}
-//         selected={selecteValues}
-//         multiple
-//       />
+  return (
+    <div style={{width: 200,}}>
+      {selecteValues.join(", ")}
+      <Tree
+        sourceData={treeArray}
+        onChange={onChange}
+        selected={selecteValues}
+        multiple
+      />
 
-//       <hr/>
+      <hr/>
 
-//       <Tree
-//         sourceData={treeArray}
-//         onChange={onChange1}
-//         selected={selectedValue}
-//       />
-//     </div>
-//   );
-// };
-// export default TreeDemo;
+      <Tree
+        sourceData={treeArray}
+        onChange={onChange1}
+        selected={selectedValue}
+      />
+    </div>
+  );
+};
+export default TreeDemo;
---
+++ lib/Tree/Tree.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import TreeItem from "./tree-item";
+import { addPrefixAndscopedClassMarker } from "../utils/classes";
+import "./tree.scss";
+
+const sc = addPrefixAndscopedClassMarker("yui-tree");
+
+const Tree: React.FC<TreeProps> = props => {
+  const { sourceData, onChange } = props;
+
+  const onItemChange = (values: string[]) => {
+    console.log("最终值", values);
+    onChange(Array.from(new Set(values)) as string[]);
+  };
+
+  return (
+    <div className={sc("")}>
+      {sourceData?.map(item => (
+        <TreeItem
+          onItemChange={onItemChange}
+          key={item.key}
+          treeProps={props}
+          item={item}
+          level={0}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Tree;
