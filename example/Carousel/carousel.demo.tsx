2088f19d7d6b57399c5f89806ccedcc8f4d055ac
“yym-yumeng123 <18026493873@163.com>
2021-11-24 00:10:43 +0800

    初步完善 demo

--- example/Carousel/carousel.demo.tsx
+++ example/Carousel/carousel.demo.tsx
@@ -2,35 +2,48 @@ import React from "react";
 import Carousel from "lib/Carousel/carousel";
 import CarouselItem from "lib/Carousel/carouselItem";
 
+// @ts-ignore
+import sz from "./image/sz.png";
+// @ts-ignore
+import xd from "./image/xd.jpg";
+// @ts-ignore
+import yy from "./image/yy.png";
+
 import "./carousel.demo.scss";
 
 const CarouselDemo = () => {
   return (
-    <>
-      <Carousel selected="3" autoPlay={true} width={500} className="你好">
+    <div className="yui-carousel-demo">
+      <Carousel selected="3" autoPlay={true}>
         <CarouselItem name="sf">
-          <div className="box">1sjkskjg </div>
+          <div className="box">1</div>
         </CarouselItem>
         <CarouselItem name="2">
-          <div className="box">dgsgsggsgsgsgsdgsg2</div>
+          <div className="box">2</div>
         </CarouselItem>
         <CarouselItem name="3">
-          <div className="box">3ss疯狂开始看</div>
+          <div className="box">3</div>
         </CarouselItem>
       </Carousel>
 
-      {/* <Carousel>
+      <Carousel width={500} className="img">
         <CarouselItem name="11">
-          <div className="box">1</div>
+          <div className="box">
+            <img src={sz} />
+          </div>
         </CarouselItem>
         <CarouselItem name="22">
-          <div className="box">2</div>
+          <div className="box">
+            <img src={xd} />
+          </div>
         </CarouselItem>
         <CarouselItem name="33">
-          <div className="box">3</div>
+          <div className="box">
+            <img src={yy} />
+          </div>
         </CarouselItem>
-      </Carousel> */}
-    </>
+      </Carousel>
+    </div>
   );
 };
 
