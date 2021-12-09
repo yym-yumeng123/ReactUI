d666b191dadc76a508128e36e8eba15a4986ff56
“yym-yumeng123 <18026493873@163.com>
2021-12-09 12:11:48 +0800

    更新 publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,4 +2,8 @@ export { default as Icon } from "./Icon/icon";
 export { default as Button } from "./Button/button";
 export { default as Card } from "./Card/card";
 export { default as Layout } from "./Layout/layout";
+export { default as Header } from "./Layout/header";
+export { default as Content } from "./Layout/content";
+export { default as Footer } from "./Layout/footer";
+export { default as SideBar } from "./Layout/sidebar";
 import "./styles/style.scss";
