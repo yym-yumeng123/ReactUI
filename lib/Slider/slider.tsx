90ae2763b74793b4917616693de26bcee6f8c495
“yym-yumeng123 <18026493873@163.com>
2021-12-27 21:26:56 +0800

    添加鼠标事件

--- lib/Slider/slider.tsx
+++ lib/Slider/slider.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, MouseEventHandler, useEffect, useRef } from "react";
 import "./slider.scss";
 
 import addPrefixAndMergeClass from "lib/Helpers/addPrefixAndMergeClass";
@@ -9,6 +9,32 @@ interface SliderProps {
 }
 
 const Slider: FC<SliderProps> = ({ initialValue = 10 }) => {
+  const dragging = useRef(false);
+
+  const onMouseDown: MouseEventHandler<HTMLDivElement> = e => {
+    dragging.current = true;
+    console.log(e.clientX);
+  };
+
+  const onMouseMove = (e: MouseEvent) => {
+    if (dragging.current) {
+      console.log(e.clientX);
+    }
+  };
+
+  const onMouseUp = (e: MouseEvent) => {
+    dragging.current = false;
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+    return () => {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+  }, []);
+
   return (
     <div className={mergeClass("wrapper")}>
       <div className={mergeClass("rail")}></div>
@@ -17,6 +43,7 @@ const Slider: FC<SliderProps> = ({ initialValue = 10 }) => {
         style={{ width: initialValue + "px" }}
       ></div>
       <div
+        onMouseDown={onMouseDown}
         className={mergeClass("bar")}
         style={{ left: initialValue + "px" }}
       ></div>
