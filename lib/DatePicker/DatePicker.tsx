484425eb57637d1f5fa04c22e5dabbe8b2e322ac
yangyumeng <yangyumeng@guanmai.cn>
2022-01-11 11:04:56 +0800

    初步吧日期弄成七周, 骨架

--- lib/DatePicker/DatePicker.tsx
+++ lib/DatePicker/DatePicker.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Input from "lib/Input/Input";
 import Icon from "lib/Icon/icon";
 
@@ -10,42 +10,68 @@ import "./datePicker.scss";
 
 const DatePicker = () => {
   const [mode, setMode] = useState<"days" | "months" | "years">("days");
-  const [visibleDays, setVisibleDays] = useState<number[]>([]); // 可见的天数
+  // const [visibleDays, setVisibleDays] = useState<number[]>([]); // 可见的天数
 
   useEffect(() => {
+    console.log(allDates, "34343");
+  }, []);
+
+  // 获取总共 42 天的数组
+  const allDates = useMemo(() => {
     const frontDays = [];
     const currentDays = [];
     const laterDays = [];
 
     const date = new Date();
-    const first = HelperDate.firstDayOfMonth(date);
-    const last = HelperDate.lastDayOfMonth(date);
-    const [year, month, day] = HelperDate.getYearMonthDate(date);
+    const first = HelperDate.firstDayOfMonth(date); // 一个月的第一天
+    const last = HelperDate.lastDayOfMonth(date); // 一个月的最后一天
+    const [year, month] = HelperDate.getYearMonthDate(date); // 当天的年月日
+
+    // 获取当月所有天数的数组
     for (let i = first.getDate(); i <= last.getDate(); i++) {
       currentDays.push(new Date(year, month, i));
     }
 
-    // 前面有几天
+    /**
+     * @description getDay() 方法根据本地时间，返回一个具体日期中一周的第几天，0 表示星期天
+     * @return 前面有几天日期
+     */
     const n = first.getDay() === 0 ? 6 : first.getDay() - 1;
 
+    // 得到当月的前一个月的剩余天数
     for (let i = 0; i <= n; i++) {
       frontDays.unshift(new Date(year, month, -i));
     }
 
-    // 后面剩几天
+    /**
+     * @description 一月跨度最多七周 42天, 后面剩余几天
+     */
     const m = 42 - currentDays.length - frontDays.length;
     for (let i = 1; i <= m; i++) {
       laterDays.push(new Date(year, month + 1, i));
     }
 
-    const finallyDays = [...frontDays, ...currentDays, ...laterDays]
-
-    console.log(currentDays, frontDays,laterDays,finallyDays, "array...");
+    return [...frontDays, ...currentDays, ...laterDays];
   }, []);
 
   const renderContent = () => {
+    const days = () => {
+      return (
+        <div className={mergeClass("content")}>
+          {HelperDate.range(1, 42 / 7).map(i => {
+            return (
+              <div>
+                {HelperDate.range(1, 7).map(day => (
+                  <span>{allDates[(i - 1) * 7 + day - 1].getDate()}</span>
+                ))}
+              </div>
+            );
+          })}
+        </div>
+      );
+    };
     const mapMode = {
-      days: <div className={mergeClass("content")}>日</div>,
+      days: days(),
       months: <div className={mergeClass("content")}>月</div>,
       years: <div className={mergeClass("content")}>年</div>
     };
--- lib/DatePicker/helper.ts
+++ lib/DatePicker/helper.ts
@@ -1,19 +1,31 @@
-
 const getYearMonthDate = (date: Date) => {
   const year = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
-  return [year, month, day]
-}
+  return [year, month, day];
+};
 
 export default {
-  firstDayOfMonth(date: Date){
-    const [year, month] = getYearMonthDate(date)
-    return new Date(year, month, 1)
+  firstDayOfMonth(date: Date) {
+    const [year, month] = getYearMonthDate(date);
+    return new Date(year, month, 1);
   },
   lastDayOfMonth(date: Date) {
-    const [year, month] = getYearMonthDate(date)
-    return new Date(year, month + 1, 0)
+    const [year, month] = getYearMonthDate(date);
+    return new Date(year, month + 1, 0);
   },
-  getYearMonthDate
-}
+  getYearMonthDate,
+  /**
+   * @param begin 开始
+   * @param end 结束
+   * @return [begin, ..., end]
+   */
+  range(begin: number, end: number): number[] {
+    const array = [];
+    for (let i = begin; i <= end; i++) {
+      array.push(i);
+    }
+
+    return array;
+  }
+};
