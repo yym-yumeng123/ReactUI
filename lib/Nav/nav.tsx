aa06702357a197a795f9c20d817a12cf0e31ef73
yym-yumeng123 <18026493873@163.com>
2023-11-15 20:40:18 +0800

    完善 items type active props

--- lib/Nav/nav.tsx
+++ lib/Nav/nav.tsx
@@ -1,9 +1,63 @@
 import * as React from "react";
+import { ReactNode } from "react";
+import addPrefixAndMergeClass from "lib/Helpers/addPrefixAndMergeClass";
+import "./nav.scss";
+const mergeClass = addPrefixAndMergeClass("yui-nav");
 
-interface NavProps {}
-
-const Nav: React.FC<NavProps> = props => {
-  return <div>Nav</div>;
+export type ItemProps = {
+  label: ReactNode;
+  key: string;
 };
 
-export default Nav;
+interface NavProps {
+  type?: "default" | "primary";
+  // 垂直很是水平
+  mode?: "hortizontal" | "vertical";
+  items: ItemProps[]; // 每一项的内容
+  activeKeys: string[];
+  onClick: (val: string) => void;
+}
+
+const NavBar: React.FC<NavProps> = (props) => {
+  const {
+    items,
+    onClick,
+    mode = "hortizontal",
+    activeKeys = [],
+    type = "default",
+  } = props;
+  console.log(items, mode, activeKeys, type);
+
+  const handleChangeActive = (item: ItemProps) => onClick(item.key);
+
+  return (
+    <div
+      className={mergeClass({
+        "": true,
+        primary: type === "primary",
+      })}
+    >
+      <div className={mergeClass("item-wrap")}>
+        <ul>
+          {items.map((item) => {
+            const { label, key } = item;
+            return (
+              <li
+                key={key}
+                onClick={() => handleChangeActive(item)}
+                className={mergeClass({
+                  item: true,
+                  active: activeKeys.indexOf(key) != -1,
+                })}
+              >
+                {label}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- views/src/example/Nav/Nav.example.tsx
+++ views/src/example/Nav/Nav.example.tsx
@@ -1,9 +1,47 @@
-import React from "react";
-import Nav from "lib/Nav/nav";
+import React, { useState } from "react";
+import NavBar, { ItemProps } from "lib/Nav/nav";
+
+const items: ItemProps[] = [
+  {
+    label: "新闻",
+    key: "news",
+  },
+  {
+    label: "游戏",
+    key: "game",
+  },
+  {
+    label: "电脑",
+    key: "computer",
+  },
+  {
+    label: (
+      <a href="http://www.baidu.com" target="_blank">
+        百度
+      </a>
+    ),
+    key: "baidu",
+  },
+];
 
 const NavExample = () => {
+  const [current, setCurrent] = useState("news");
+  const handleChange = (val: string) => {
+    setCurrent(val);
+  };
   return (
-    <Nav />
+    <>
+      <NavBar items={items} activeKeys={[current]} onClick={handleChange} />
+
+      <br />
+
+      <NavBar
+        items={items}
+        activeKeys={[current]}
+        onClick={handleChange}
+        type="primary"
+      />
+    </>
   );
 };
 
