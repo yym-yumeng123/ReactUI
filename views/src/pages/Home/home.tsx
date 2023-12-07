355ec500dc949d0ea84c542e52dc5453371c8d4d
yym-yumeng123 <18026493873@163.com>
2023-12-07 17:22:56 +0800

    修改文档样式

--- views/src/pages/Home/home.tsx
+++ views/src/pages/Home/home.tsx
@@ -2,18 +2,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./home.scss";
 import Button from "lib/Button/button";
+import { Card } from "lib";
+import Row, { Col } from "lib/Grid/row";
 
 export default function Home() {
   return (
     <div className="v-home-wrap">
-      <div className="v-home">
-        <h1>YUI组件库</h1>
-        <p>基于 React + TypeScript, 面向开发者的组件库</p>
-      </div>
-      <div className="v-home-link">
-        <Link to={`/overview/icon`}>
-          <Button level="primary">组件</Button>
-        </Link>
+      <header>
+        <div className="v-home">
+          <h1>YUI组件库</h1>
+          <pre>基于 React, 面向开发者的组件库</pre>
+        </div>
+        <div className="v-home-link">
+          <Link to={`/overview/icon`}>
+            <Button level="primary">立即前往</Button>
+          </Link>
+        </div>
+      </header>
+
+      <div className="v-card">
+        <Row gutter={20}>
+          <Col span={8}>
+            <Card title="组件丰富">
+              <pre>完成 20+ 组件</pre>
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="技术栈">
+              <ul>
+                <li>
+                  <pre>React + Hooks</pre>
+                </li>
+                <li>
+                  <pre>TypeScript</pre>
+                </li>
+                <li>
+                  <pre>Webpack</pre>
+                </li>
+              </ul>
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="源码">
+              <pre>代码简洁易懂</pre>
+              <pre>尽量不引入外部库</pre>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   );
