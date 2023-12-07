6db589c52f5b1049305720f72441cb5b4686bff9
yym-yumeng123 <18026493873@163.com>
2023-12-07 17:56:22 +0800

    add webpack doc 命令

---
+++ webpack.doc.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const common = require('./webpack.common')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+
+module.exports = merge(common, {
+  mode: 'production',
+  entry: {
+    index: './views/view.tsx'
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "doc"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "YUI组件库",
+      template: './views/view.html',
+      filename: "view.html"
+    }),
+  ],
+})
